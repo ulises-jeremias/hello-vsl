d966a0ebf1847ba33da2146b5fb3b8d6dac45b63
ulises-jeremias <ulisescf.24@gmail.com>
2024-06-09 18:49:44 -0300

    chore: Update devcontainer.json, v.mod, and main.v files
    The commit updates the devcontainer.json file, v.mod file, and main.v file.

--- main.v
+++ main.v
@@ -9,16 +9,16 @@ fn pow_function(x f64, _ []f64) f64 {
 }
 
 fn main() {
-	f := func.new_func(f: pow_function)
+	f := func.Fn.new(f: pow_function)
 	println('f(x) = x^(3/2)')
 	mut expected := 1.5 * math.sqrt(2.0)
 	mut result, mut abserr := deriv.central(f, 2.0, 1e-8)
 	println('x = 2.0')
-	println("f'(x) = $result +/- $abserr")
-	println('exact = $expected')
+	println("f'(x) = ${result} +/- ${abserr}")
+	println('exact = ${expected}')
 	expected = 0.0
 	result, abserr = deriv.forward(f, 0.0, 1e-8)
 	println('x = 0.0')
-	println("f'(x) = $result +/- $abserr")
-	println('exact = $expected')
+	println("f'(x) = ${result} +/- ${abserr}")
+	println('exact = ${expected}')
 }
--- v.mod
+++ v.mod
@@ -1,6 +1,6 @@
 Module {
         name: 'hello-vsl'
-        version: '0.1.0'
+        version: '0.1.2'
         license: 'MIT'
 	      repo_url: 'https://github.com/ulises-jeremias/hello-vsl'
         dependencies: ['vsl']
